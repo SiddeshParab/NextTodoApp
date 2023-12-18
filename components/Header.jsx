2ed46be93a75bf4fc7772d58440bc1cf8bb7060c
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 03:11:04 +0530

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from './NavBar';
+
+const Header = () => {
+  return (
+    <header className='w-full flex items-center justify-center bg-slate-500'>
+      <NavBar />
+    </header>
+  );
+};
+
+export default Header;
